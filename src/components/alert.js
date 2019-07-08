e9ba824cfdbc6df18e8fc70da6d84266a143b569
Justin Juno <justin@edtechdeveloper.com>
2019-07-08 15:22:34 -0500

    create alert component

---
+++ src/components/alert.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function Alert() {
+  return (
+    <div className="alert">
+      <div className="container">
+        <small>Are you hiring? I'm seeking remote opportunities!</small>
+      </div>
+    </div>
+  )
+}
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,9 +1,15 @@
 import React from "react"
 import PropTypes from "prop-types"
 import "../styles/app.scss"
+import Alert from "./alert"
 
 export default function Layout({ children }) {
-  return <div>{children}</div>
+  return (
+    <div>
+      <Alert />
+      {children}
+    </div>
+  )
 }
 
 Layout.propTypes = {
